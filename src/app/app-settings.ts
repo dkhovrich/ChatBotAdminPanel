d020e6f70b6cb55d0e3404fa6caafb4fb7ec84c7
Dmitriy Khovrich <khovrych.d@gmail.com>
2018-07-24 10:35:48 +0300

    Prepare app for heroku (#14)
    * Prepare app for heroku
    * remove build script

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,3 +1,1 @@
-export const BASE_URL = 'https://boiling-brushlands-26824.herokuapp.com/admin';
-export const BASE_URL_DEVELOPMENT = 'http://localhost:3000/admin';
 export const DEFAULT_DEV_EMAIL = 'admin@admin.com';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://boiling-brushlands-26824.herokuapp.com/admin'
 };

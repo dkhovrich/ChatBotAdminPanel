1dd08b06f6e437b5bb30ff829069ef99a92cdc29
Dmitriy Khovrich <dkhovrich@lohika.com>
2018-07-24 10:33:49 +0300

    Prepare app for heroku

--- src/interceptors/base-url.interceptor.ts
+++ src/interceptors/base-url.interceptor.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
-import { isDevMode } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as urlJoin from 'url-join';
 
-import { BASE_URL, BASE_URL_DEVELOPMENT } from '../app/app-settings';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const baseUrl: string = isDevMode() ? BASE_URL_DEVELOPMENT : BASE_URL;
-    req = req.clone({ url: urlJoin(baseUrl, req.url) });
-
+    req = req.clone({ url: urlJoin(environment.baseUrl, req.url) });
     return next.handle(req);
   }
 }
